273ba64aab6274d68ba86efee6a81d6644477a15
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-08-25 22:14:24 -0400

    updated askpage component to make post calls to backend

--- passion-project-front-end/src/AskPage.js
+++ passion-project-front-end/src/AskPage.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import {useState} from "react";
 const TitleInput = styled.input`
 width: 100%;
 height: 30px;
 margin-bottom: 10px;
 margin-top: 30px;
-border: solid 2px black;
+border: solid 2px gray;
 border-radius: 5px;
 color: black;
 `
@@ -13,7 +14,7 @@ const TextAreaTag = styled.textarea`
 width: 100%;
 height: 100%;
 padding: 10px;
-border: solid 2px black;
+border: solid 2px gray;
 border-radius: 5px;
 
 `
@@ -44,13 +45,35 @@ position: absolute;
   margin: -100px 0 0 -150px;
 `
 function AskPage(){
+const [question, setQuestion] = useState("");
+const [topic, setTopic] = useState("");
+
+const handleTopicChange = event => setTopic(event.target.value);
+  const handleQuestionChange = event => setQuestion(event.target.value)
+
+const handleSubmitClick = ()=>{
+      
+    fetch('http://localhost:8080/api/questions', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+            title: topic,
+            question: question,
+            createdDate: "2024-08-26"})
+      }).then(res => res.json())
+        .then(res => console.log(res));
+
+}
 return(
     <>
     <CenterPageDiv>
         <H1Tag>Ask A Question</H1Tag>
-    <TitleInput placeholder="Title of your Question"></TitleInput>
-    <TextAreaTag placeholder="Enter Question here"></TextAreaTag>
-    <SubmitButton>Submit</SubmitButton>
+    <TitleInput placeholder="Title of your Question" onChange={handleTopicChange}></TitleInput>
+    <TextAreaTag placeholder="Enter Question here" onChange={handleQuestionChange}></TextAreaTag>
+    <SubmitButton onClick={handleSubmitClick}>Submit</SubmitButton>
     </CenterPageDiv>
     </>
 )
