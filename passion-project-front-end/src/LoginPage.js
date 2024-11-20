5a32ac877273598e81686ef569d20d811f3975ae
Mohammed N <mnaser11218@gmail.com>
2024-11-20 10:27:30 -0500

    added logic for login inputs

--- passion-project-front-end/src/LoginPage.js
+++ passion-project-front-end/src/LoginPage.js
@@ -1,20 +1,39 @@
 import styled from "styled-components";
+import { Component } from "react";
 import H1HeaderTag from "./styled-components/H1HeaderTag";
 import StyledInput from "./styled-components/StyledInput";
-
-function LoginPage(){
+import BlueButton from "./styled-components/BlueButton";
 const CenterPageDiv = styled.div`
 padding: 30px 28px;
 `
-return (
-    <>
-    <CenterPageDiv>
-    <H1HeaderTag>Login </H1HeaderTag>
-    <StyledInput/>
-    </CenterPageDiv>
+ class LoginPage extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            userName: '',
+            password: '',
+        }
+    }
 
+render(){
+    return (
+        <>
+        <CenterPageDiv>
+        <H1HeaderTag>Login </H1HeaderTag>
+        <StyledInput placeholder="email" type="email" value={this.state.userName}
+        onChange={e=>this.setState({userName: e.target.value})}
+        />
+        <StyledInput placeholder="password" type="password" value={this.state.password} 
+        onChange={e=>this.setState({password: e.target.value})}
+        />
+        <BlueButton>Login</BlueButton>
+        </CenterPageDiv>
     
-    </>
-)
+        
+        </>
+    )
+}
+
+
 }
 export default LoginPage;
--- passion-project-front-end/src/styled-components/BlueButton.js
+++ passion-project-front-end/src/styled-components/BlueButton.js
@@ -5,7 +5,9 @@ background-color: #378ad3;
 color:#fff;
 border:0;
 border-radius: 5px;
-padding:12px 10px;
+padding:12px 20px;
 text-decoration: none;
+font-size: 1.1rem;
+cursor: pointer;
 `
 export default BlueButton;
