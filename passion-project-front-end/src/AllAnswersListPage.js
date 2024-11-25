7e049190e59f6b093190e7962c9b5f4ade4e4c94
Mohammed N <mnaser11218@gmail.com>
2024-11-24 20:44:45 -0500

    added logic to get answers of question in allanswerslistpage

--- passion-project-front-end/src/AllAnswersListPage.js
+++ passion-project-front-end/src/AllAnswersListPage.js
@@ -1,10 +1,23 @@
 import styled from "styled-components";
-
+import { useEffect, useState } from "react";
 function AllAnswersListPage({...props}){
     const {questionId} = props;
+    const [answers, setAnswers ] = useState([]);
+    const getAnswers = ()=>{
+        fetch(`http://localhost:8080/api/answers/question/${questionId}`)
+        .then(res=> res.json())
+        .then(data=> setAnswers(data))
+    }
+   
+    useEffect(()=>{
+    getAnswers();
+    },[])
 
     return (<>
-    <div>All Answers List Page </div>
+    <div>{answers.map(answer=> <div>
+
+        {answer.answer}
+    </div>)}</div>
     </>)
 }
 export default AllAnswersListPage;
