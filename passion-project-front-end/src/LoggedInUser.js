b18fa01cb51d433229ff838682debed12758a543
Mohammed N <mnaser11218@gmail.com>
2024-11-27 16:29:00 -0500

    updated navigate method in loggedin user when logging out

--- passion-project-front-end/src/LoggedInUser.js
+++ passion-project-front-end/src/LoggedInUser.js
@@ -4,17 +4,20 @@ import { useContext } from "react";
 // import UserContext from "./UserContext";
 import { useUser } from "./CurrentUser";
 import BlueButton from "./styled-components/BlueButton";
+import { useNavigate } from "react-router-dom";
 
 
 function LoggedInUser(){
     // const user = useContext(UserContext)
+    const navigate = useNavigate()
     const { setCurrentLoggedInUser } = useUser()
     const handleLogOut = (e)=> {
         e.preventDefault()
         setCurrentLoggedInUser(null);
+        navigate('/home')
     }
 
-    return (<H1HeaderTag >Welcome
+    return (<H1HeaderTag> Welcome
     <hr></hr>
     <BlueButton onClick={handleLogOut}>Log Out</BlueButton>
     </H1HeaderTag>)
