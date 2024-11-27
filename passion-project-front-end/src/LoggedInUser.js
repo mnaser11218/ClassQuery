354656c429f6046275838bacc4c144f91ebab0b0
Mohammed N <mnaser11218@gmail.com>
2024-11-27 15:47:02 -0500

    configured logout functionality for user

--- passion-project-front-end/src/LoggedInUser.js
+++ passion-project-front-end/src/LoggedInUser.js
@@ -1,11 +1,23 @@
 import styled from "styled-components"
 import H1HeaderTag from "./styled-components/H1HeaderTag"
 import { useContext } from "react";
-import UserContext from "./UserContext";
+// import UserContext from "./UserContext";
+import { useUser } from "./CurrentUser";
+import BlueButton from "./styled-components/BlueButton";
 
 
 function LoggedInUser(){
-    const user = useContext(UserContext)
-    return (<H1HeaderTag >Logged in user page. User id is:  {user}</H1HeaderTag>)
+    // const user = useContext(UserContext)
+    const { setCurrentLoggedInUser } = useUser()
+    const handleLogOut = (e)=> {
+        e.preventDefault()
+        console.log("clicked logout")
+        setCurrentLoggedInUser(null);
+    }
+
+    return (<H1HeaderTag >Welcome
+    <hr></hr>
+    <BlueButton onClick={handleLogOut}>Log Out</BlueButton>
+    </H1HeaderTag>)
 }
 export default LoggedInUser;
