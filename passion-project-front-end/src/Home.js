9db7fbc9c77a74e1e480c50ddba32dd2323478cc
Mohammed N <mnaser11218@gmail.com>
2024-12-17 22:25:54 -0500

    fetched assignments in home component

--- passion-project-front-end/src/Home.js
+++ passion-project-front-end/src/Home.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 const H1home = styled.div`
 color:black;
 font-size: 5rem;
@@ -26,36 +27,25 @@ height: 400px;
   //background-image: url("https://st3.depositphotos.com/7865540/12756/i/450/depositphotos_127563976-stock-photo-notepad-with-text-in-frame.jpg")
 `
 function Home(){
+  const [assignments, setAssignments]=useState([])
+
+  useEffect(()=>{
+  fetch("http://localhost:8080/api/assignments")
+  .then(response=> response.json())
+  .then(assign=> setAssignments(assign))
+  },[])
 return(
     <>
     <DivEle>
-    <AssignmentEle>testing</AssignmentEle>
-    <AssignmentEle>testing</AssignmentEle>
-    <AssignmentEle>testing</AssignmentEle>
-    <AssignmentEle>testing</AssignmentEle>
-    {/* <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/>
-    <AssignmentEle/> */}
+    {assignments.map(ele=>{
+      return(
+      <AssignmentEle>
+        {ele.name} 
+    
+      </AssignmentEle>
+
+    )}
+      )} 
     </DivEle>
     
     </>
