c739b26459bba7939e2a50ff45d7265f1e7d2429
Mohammed N <mnaser11218@gmail.com>
2024-12-21 10:18:24 -0500

    added http request to add assignment from frontend

--- passion-project-front-end/src/AddAssignment.jsx
+++ passion-project-front-end/src/AddAssignment.jsx
@@ -3,6 +3,8 @@ import H1HeaderTag from './styled-components/H1HeaderTag'
 import StyledInput from './styled-components/StyledInput'
 import BlueButton from './styled-components/BlueButton'
 import styled from 'styled-components'
+import { useUser } from './CurrentUser'
+import { useNavigate } from 'react-router-dom'
 
 const CenterPageDiv = styled.div`
 padding: 30px 28px;
@@ -12,27 +14,59 @@ export default function AddAssignment() {
     const [topic, setTopic] = useState("")
     const [courseName, setCourseName] = useState("")
     const [courseDescription, setCourseDescription] = useState("")
-    const handleAddAssignment = ()=>{};
+    const {currentLoggedInUser} = useUser();
+    const navigate = useNavigate()
+    const routeChange = ()=>{
+        let path = `/`
+        navigate(path)
+    }
+    
+
+    const handleAddAssignment = ()=>{
+
+        fetch("http://localhost:8080/api/assignments", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                "name": name,
+                "topic": topic,
+                "courseName": courseName,
+                "description": currentLoggedInUser.name,
+                "created": new Date(),
+                "questions": null
+            })
+        })
+    .then(response=> response.json())
+    .then(data=> {
+        console.log(data)
+        routeChange()
+
+    })
+
+
+
+    };
   return (
     <>
     <CenterPageDiv>
     <H1HeaderTag>Add Assignment </H1HeaderTag>
     
-    {/* make this for the owner name
-    <StyledInput placeholder="name" type="name" value={name}
+    <StyledInput placeholder="Assignment name" type="name" value={name}
     onChange={e=>setName(e.target.value)}
-    /> */}
-    <StyledInput placeholder="Assignment Name" type="topic" value={topic}
+    />
+    <StyledInput placeholder="Topic" type="topic" value={topic}
     onChange={e=>setTopic(e.target.value)}
     />
     <StyledInput placeholder="Course Name" type="courseName" value={courseName} 
  
     onChange={e=>setCourseName(e.target.value)}
     />
-      <StyledInput placeholder="Assignment Description" type="courseDescription" value={courseDescription} 
+      {/* <StyledInput placeholder="Assignment Description" type="courseDescription" value={courseDescription} 
     
     onChange={e=>setCourseDescription(e.target.value)}
-    />
+    /> */}
     <BlueButton onClick={handleAddAssignment}>Save Assignment</BlueButton>
     </CenterPageDiv>
 
