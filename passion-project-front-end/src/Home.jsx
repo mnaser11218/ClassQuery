19c7261a6dff74b374df220631889bb7bcd0b782
Mohammed N <mnaser11218@gmail.com>
2024-12-23 22:29:10 -0500

    styled course assignments

--- passion-project-front-end/src/Home.jsx
+++ passion-project-front-end/src/Home.jsx
@@ -13,12 +13,12 @@ border: solid 2px white;
 const DivEle = styled.div`
 color: black;
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 15px;
   padding: 10px;
 
   // overflow: auto;
-  // height: 300px;
+   //height: 1000px;
  
 `
 const LinkTag =styled(Link)`
@@ -26,9 +26,9 @@ const LinkTag =styled(Link)`
 border-radius: 5px;
 display: inline-block;
 margin:auto;
-  font-family: 'Lora', serif;
-      font-weight: 700;
-      font-size: 1.5rem;
+
+      font-weight: 550;
+      font-size: 1.3rem;
       color: #34495e;
 text-decoration: none;
 background-color:white;
@@ -43,7 +43,7 @@ border: solid 2px gray;
   padding: 20px 0;
   font-size: 30px;
 width: 100%;
-height: 300px;
+height: 250px;
  border-radius: 10px;
   //background-image: url("https://st3.depositphotos.com/7865540/12756/i/450/depositphotos_127563976-stock-photo-notepad-with-text-in-frame.jpg")
 `
@@ -85,7 +85,7 @@ return(
     
     {assignments?.map(ele=>{
       return(
-      <AssignmentEle style={{"backgroundImage": `url(${ele.description})`,   backgroundRepeat: "no-repeat",
+      <AssignmentEle style={{"backgroundImage": `url(https://www.shutterstock.com/image-illustration/assignment-technical-squares-600nw-702799699.jpg)`,   backgroundRepeat: "no-repeat",
       // Optional: Makes sure the image covers the element
       backgroundPosition: "center"}}>
         <LinkTag to={`/assignmentquestion/${ele.id}`}>
