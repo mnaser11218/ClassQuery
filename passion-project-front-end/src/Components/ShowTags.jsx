77c5a60f9d5b843c660469d0645193cd44ddf57f
Mohammed N <mnaser11218@gmail.com>
2024-12-26 10:55:49 -0500

    added style to showtags component

--- passion-project-front-end/src/Components/ShowTags.jsx
+++ passion-project-front-end/src/Components/ShowTags.jsx
@@ -11,27 +11,31 @@ border: solid 2px white;
 `
 
 const DivEle = styled.div`
-color: black;
+// color: black;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 15px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
   padding: 10px;
 
   // overflow: auto;
-   //height: 1000px;
- 
+   //height: 1000px; 
 `
+const Description = styled.p`
+font-size: 15px;
+padding: 10px;
+
+`
+
 const LinkTag =styled(Link)`
 
 border-radius: 5px;
 display: inline-block;
 margin:auto;
-
+color:  #F8F7E5;
       font-weight: 600;
       font-size: 1.5rem;
-      color: #34495e;
 text-decoration: none;
-background-color:white;
+// background-color:white;
 padding: 5px;
 // margin-bottom: 30px;
 
@@ -39,18 +43,18 @@ padding: 5px;
 
 const AssignmentEle = styled.div`
 border: solid 2px gray;
- background-color: white;
-  text-align: center;
-  padding: 20px 0;
+//  background-color: white;
+  //text-align: center;
+  padding: 10px 0;
   font-size: 30px;
 width: 100%;
-height: 250px;
+height: 200px;
  border-radius: 10px;
   //background-image: url("https://st3.depositphotos.com/7865540/12756/i/450/depositphotos_127563976-stock-photo-notepad-with-text-in-frame.jpg")
 `
 
 const StyledHeader = styled.a`
-color: #F8F7E5;
+// color: #F8F7E5;
 font-size: 2rem;
 font-family: var(--ff-sans);
 cursor: pointer;
@@ -66,7 +70,7 @@ function ShowTags(){
   const { currentLoggedInUser } = useUser()
   let navigate = useNavigate(); 
   const routeChange = ()=> {
-      let path = `/addassignment`; 
+      let path = `/addtag`; 
       navigate(path);
   }
 
@@ -86,12 +90,12 @@ return(
     
     {assignments?.map(ele=>{
       return(
-      <AssignmentEle style={{"backgroundImage": `url(https://www.shutterstock.com/image-illustration/assignment-technical-squares-600nw-702799699.jpg)`,   backgroundRepeat: "no-repeat",
-      // Optional: Makes sure the image covers the element
-      backgroundPosition: "center"}}>
+      <AssignmentEle>
         <LinkTag to={`/assignmentquestion/${ele.id}`}>
-        {<br></br>} {ele.tagName}
+        <span className="tag">{ele.tagName}</span>
         </LinkTag>
+
+        <Description>{ele.tagDescription}</Description>
          
     
       </AssignmentEle>
