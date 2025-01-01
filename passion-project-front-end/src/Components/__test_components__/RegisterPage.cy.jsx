357af31b293d79dbeae6c5a71e4750e78c47de05
Mohammed N <mnaser11218@gmail.com>
2025-01-01 14:11:36 -0500

    updated register test component

--- passion-project-front-end/src/Components/__test_components__/RegisterPage.cy.jsx
+++ passion-project-front-end/src/Components/__test_components__/RegisterPage.cy.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable no-undef */
 import React from 'react'
 import RegisterPage from '../RegisterPage'
+import { UserProvider } from '../../CurrentUser'
+import GlobalStyles from '../../styled-components/GlobalStyles'
+import WholePage from '../../styled-components/WholePageDivStyle'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('<RegisterPage />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<RegisterPage />)
+    cy.mount(
+      <BrowserRouter>
+      <WholePage>
+        <GlobalStyles/>
+        <UserProvider>
+        <RegisterPage />
+       </UserProvider>
+       </WholePage>
+       </BrowserRouter>
+    
+  
+  )
   })
 })
